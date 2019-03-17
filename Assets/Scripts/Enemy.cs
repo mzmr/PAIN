de6d787c18fa7ac372828c37bbcfdf8a9b8b54b5
PauulM <pawmat@student.agh.edu.pl>
2019-03-17 13:13:13 +0100

    Prepare Enemy and Player damage stats

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public float InitHealth;
+
+    public float Damage;
+
+    public float MoveSpeed;
+
+    public GameObject TargetToFollow;
+
+    public double MinDistanceFromTargetToMove;
+
+    public double MaxDistanceFromTargetToMove;
+
+    protected Rigidbody2D rigidBody;
+
+    protected double distanceFromTarget;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+
+    protected abstract Vector3 GetUpdatedVelocity();
+
+    protected double CalculateDistanceFromTarget()
+    {
+        return Math.Sqrt(
+            Math.Pow(Convert.ToDouble(TargetToFollow.transform.position.x) - Convert.ToDouble(transform.position.x), 2)
+            + Math.Pow(Convert.ToDouble(TargetToFollow.transform.position.y) - Convert.ToDouble(transform.position.y), 2)
+        );
+    }
+
+    protected Vector3 CalculateVelocityToTarget()
+    {
+        return new Vector3(
+            TargetToFollow.transform.position.x - transform.position.x,
+            TargetToFollow.transform.position.y - transform.position.y,
+            0f
+        );
+    }
+}
--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -3,54 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slime : MonoBehaviour
+public class Slime : Enemy
 {
-    public float MoveSpeed;
-    public GameObject TargetToFollow;
-    public double minDistanceFromTargetToMove;
-    public double maxDistanceFromTargetToMove;
-    private Rigidbody2D rigidBody;
-    private double distanceFromTarget;
 
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
-    {      
+    protected override void Update()
+    {
+        base.Update();
         rigidBody.velocity = GetUpdatedVelocity();
     }
 
-    private Vector3 GetUpdatedVelocity()
+    protected override Vector3 GetUpdatedVelocity()
     {
         double distanceFromTarget = CalculateDistanceFromTarget();
-        if (distanceFromTarget < minDistanceFromTargetToMove || maxDistanceFromTargetToMove < distanceFromTarget)
+        if (distanceFromTarget < MinDistanceFromTargetToMove || MaxDistanceFromTargetToMove < distanceFromTarget)
         {
             return new Vector3(0f, 0f, 0f);
         }
         var newVelocity = CalculateVelocityToTarget().normalized;
         newVelocity *= MoveSpeed;
         return newVelocity;
-    }
-
-    private double CalculateDistanceFromTarget()
-    {
-        return Math.Sqrt(
-            Math.Pow(Convert.ToDouble(TargetToFollow.transform.position.x) - Convert.ToDouble(transform.position.x), 2)
-            + Math.Pow(Convert.ToDouble(TargetToFollow.transform.position.y) - Convert.ToDouble(transform.position.y), 2)
-        );
-    }
-
-    private Vector3 CalculateVelocityToTarget()
-    {
-        return new Vector3(
-            TargetToFollow.transform.position.x - transform.position.x,
-            TargetToFollow.transform.position.y - transform.position.y,
-            0f
-        );
     }
 
 }
