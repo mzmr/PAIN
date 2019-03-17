186bf6faa6bf69d3e3a3972c47a4a661873b5720
Maciej Znamirowski <maciek@znamirowski.pl>
2019-03-17 21:01:44 +0100

    hiding user behind the trees, made player collider smaller

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour, IComparable<Obstacle>
+{
+    public SpriteRenderer MySpriteRenderer { get; set; }
+
+    public int CompareTo(Obstacle other)
+    {
+        return MySpriteRenderer.sortingOrder - other.MySpriteRenderer.sortingOrder;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
