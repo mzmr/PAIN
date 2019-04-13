479c81604a40cfec14b3bbcb982dc99bdc8fc3c4
Maciej Znamirowski <maciek@znamirowski.pl>
2019-04-13 14:51:59 +0200

    Added tree tiles and created a map

--- Assets/Scripts/TileScripts/TreeTile.cs
+++ Assets/Scripts/TileScripts/TreeTile.cs
@@ -1,8 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class TreeTile : MonoBehaviour
+public class TreeTile : Tile
 {
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        go.GetComponent<SpriteRenderer>().sortingOrder = -position.y * 2;
 
+        return base.StartUp(position, tilemap, go);
+    }
+
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/Tiles/TreeTile")]
+    public static void CreateTreeTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Treetile", "New Treetile", "asset", "Save treetile", "Assets");
+        if (path == "")
+        {
+            return;
+        }
+
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<TreeTile>(), path);
+    }
+#endif
 }
