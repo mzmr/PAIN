782a40eb739217c80d666b09736239b41f0a6f1e
PauulM <pawmat@student.agh.edu.pl>
2019-04-28 12:03:09 +0200

    Create money amount stat

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    public Text MoneyText;
+    public int CurrentGoldAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoneyText.text = CurrentGoldAmount.ToString();
+    }
+}
